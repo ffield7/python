b4a665e15529f42880d261215d2eeec797d3795b
ffield7 <43576477+ffield7@users.noreply.github.com>
2021-08-31 13:36:37 +0900

    Update test_read_write exif1.py

--- test_read_write exif1.py
+++ test_read_write exif1.py
@@ -3,7 +3,6 @@ import glob
 for name in glob.glob('./*.jpg'):
     img = pyexiv2.Image(name)
     metadata = img.read_exif()
-# metadata.read()
     str1=name.split('_')[1]
     str2=str1[0:4]+':'+str1[4:6]+':'+str1[6:8]+' '+str1[9:11]+':'+str1[11:13]+':'+str1[13:15]
     print(str2)
